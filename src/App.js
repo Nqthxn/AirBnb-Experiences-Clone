ec491fc83c15105b704ee2bf260826e8f73c9515
Nathan Lam <nathanl1605@gmail.com>
2024-07-21 16:11:29 +0100

    Utilized React's prop functionality to provide each card with unique information

--- src/App.js
+++ src/App.js
@@ -1,14 +1,58 @@
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import Card from './components/Card';
+import Image1 from './Images/card-image1.webp';
+import Image2 from './Images/card-image2.webp';
+import Image3 from './Images/card-image3.webp';
+import Image4 from './Images/card-image4.webp';
+import Image5 from './Images/card-image5.webp';
+import Image6 from './Images/card-image6.webp';
 
 function App(){
-  //<Hero />
+
   return(
     <div>
       <Navbar />
       <Hero />
-      <Card />
+      <div className="display-cards">
+        <Card 
+          img={Image1}
+          rating="4.93 (1,262) &bull; Galway"
+          description="Galway's legendary walking tour!"
+          price="From &euro; 15 / person"
+        />
+        <Card 
+          img={Image2}
+          rating="4.91 (208) &bull; Fethard-On-Sea"
+          description="Sea Cave Kayaking Experience"
+          price="Hosted by Graham"
+        />
+        <Card 
+          img={Image3}
+          rating="4.93 (750) &bull; Dublin"
+          description="Original Dublin Walking Tour"
+          price="From &euro; 23 / person"
+        />
+        <Card 
+          img={Image4}
+          rating="4.91 (1,012) &bull; Dublin"
+          description="Irish Food Trail"
+          price="From &euro; 89 / person"
+        />
+        <Card 
+          img={Image5}
+          rating="5.0 (142) &bull; Galway"
+          description="The Irish Dance Experience"
+          price="From &euro; 30 / person"
+        />
+        <Card 
+          img={Image6}
+          rating="4.99 (537) &bull; Waterville"
+          description="Sheep Herding Demonstration"
+          price="From &euro; 20 / person"
+        />
+
+      </div>
     </div>
   );
 }
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,26 +1,19 @@
-import image1 from '../Images/card-image1.webp';
 import star from '../Images/star.png';
 
-function Card(){
+function Card(prop){
     return(
         <div className="card-container">
             <div className="image-container">
-                <img src={image1} className="card-image"/>
+                <img src={prop.img} className="card-image"/>
             </div>
             <div className="info-container">
                 <p className="review-container">
                     <img src={star} className="star"/>
-                    <span>4.93 (1,262) &bull; Galway</span>
+                    <span>{prop.rating}</span>
                 </p>
-                <p className="card-description">
-                    Galway's legendary walking tour!
-                </p>
-                <p> 
-                    From &euro; 15 / person
-                </p>
-
+                <p className="card-description">{prop.description}</p>
+                <p>{prop.price}</p>
             </div>
-
         </div>
     );
 }
